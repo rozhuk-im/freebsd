aae3a965e0d974e33fb95fbefdb692a0a28e23a2
Rozhuk Ivan <rozhuk.im@gmail.com>
2025-06-08 20:21:40 +0300

    Revert "Convert libllvm, libclang and liblldb into private shared libraries"
    This reverts commit a743b3f1a4c538c50236b5322d4c82db08a3d9f2.

--- usr.bin/clang/llvm.prog.mk
+++ usr.bin/clang/llvm.prog.mk
@@ -10,19 +10,15 @@ CFLAGS+=	-I${OBJTOP}/lib/clang/libllvm
     (${PROG_CXX} == "clang-tblgen" || ${PROG_CXX} == "lldb-tblgen" || \
      ${PROG_CXX} == "llvm-min-tblgen" || ${PROG_CXX} == "llvm-tblgen")
 LIBDEPS+=	llvmminimal
-LIBPRIV=
-LIBEXT=		a
 .else
 LIBDEPS+=	llvm
-LIBPRIV=	private
-LIBEXT=		so
 LIBADD+=	z
 LIBADD+=	zstd
 .endif
 
 .for lib in ${LIBDEPS}
-DPADD+=		${OBJTOP}/lib/clang/lib${lib}/lib${LIBPRIV}${lib}.${LIBEXT}
-LDADD+=		${OBJTOP}/lib/clang/lib${lib}/lib${LIBPRIV}${lib}.${LIBEXT}
+DPADD+=		${OBJTOP}/lib/clang/lib${lib}/lib${lib}.a
+LDADD+=		${OBJTOP}/lib/clang/lib${lib}/lib${lib}.a
 .endfor
 
 PACKAGE=	clang
