7607cd04d1996d52f4a89844b06373fb3c114f37
Rozhuk Ivan <rozhuk.im@gmail.com>
2026-01-28 03:40:38 +0200

    CARP: move CARP MAC address to const

--- sys/netinet/ip_carp.c
+++ sys/netinet/ip_carp.c
@@ -178,6 +178,10 @@ struct carpkreq {
 	uint16_t	carpr_vrrp_adv_inter;
 };
 
+
+static const uint8_t carp_mac_tmpl[5] = { 0x00, 0x00, 0x5e, 0x00, 0x01 };
+
+
 /*
  * Brief design of carp(4).
  *
@@ -1737,9 +1741,8 @@ int
 carp_forus(struct ifnet *ifp, u_char *dhost)
 {
 	struct carp_softc *sc;
-	uint8_t *ena = dhost;
 
-	if (ena[0] || ena[1] || ena[2] != 0x5e || ena[3] || ena[4] != 1)
+	if (bcmp(dhost, carp_mac_tmpl, sizeof(carp_mac_tmpl)))
 		return (0);
 
 	CIF_LOCK(ifp->if_carp);
@@ -2046,6 +2049,7 @@ carp_output(struct ifnet *ifp, struct mbuf *m, const struct sockaddr *sa)
 {
 	struct m_tag *mtag;
 	int vhid;
+	struct ether_header *eh;
 
 	if (!sa)
 		return (0);
@@ -2063,33 +2067,27 @@ carp_output(struct ifnet *ifp, struct mbuf *m, const struct sockaddr *sa)
 		return (0);
 	}
 
-	mtag = m_tag_find(m, PACKET_TAG_CARP, NULL);
-	if (mtag == NULL)
-		return (0);
-
-	bcopy(mtag + 1, &vhid, sizeof(vhid));
-
-	/* Set the source MAC address to the Virtual Router MAC Address. */
+	/* Check interface type. */
 	switch (ifp->if_type) {
 	case IFT_ETHER:
 	case IFT_BRIDGE:
-	case IFT_L2VLAN: {
-			struct ether_header *eh;
-
-			eh = mtod(m, struct ether_header *);
-			eh->ether_shost[0] = 0;
-			eh->ether_shost[1] = 0;
-			eh->ether_shost[2] = 0x5e;
-			eh->ether_shost[3] = 0;
-			eh->ether_shost[4] = 1;
-			eh->ether_shost[5] = vhid;
-		}
+	case IFT_L2VLAN:
 		break;
 	default:
 		printf("%s: carp is not supported for the %d interface type\n",
 		    if_name(ifp), ifp->if_type);
 		return (EOPNOTSUPP);
 	}
+
+	/* Set the source MAC address to the Virtual Router MAC Address. */
+	mtag = m_tag_find(m, PACKET_TAG_CARP, NULL);
+	if (mtag == NULL)
+		return (0);
+
+	eh = mtod(m, struct ether_header *);
+	bcopy(mtag + 1, &vhid, sizeof(vhid));
+	bcopy(carp_mac_tmpl, eh->ether_shost, sizeof(carp_mac_tmpl));
+	eh->ether_shost[5] = vhid;
 
 	return (0);
 }
